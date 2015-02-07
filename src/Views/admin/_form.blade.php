b42f62ac70ccb4677c729fa6a702e9063926f2eb
Samuel De Backer <sdebacker@gmail.com>
2015-02-07 10:46:11 +0100

    form

--- src/Views/admin/_form.blade.php
+++ src/Views/admin/_form.blade.php
@@ -15,28 +15,20 @@
 @include('core::admin._image-fieldset', ['field' => 'image'])
 
 <div class="row">
-    <div class="col-sm-4 form-group @if($errors->has('start_date'))has-error @endif">
-        {{ Form::label('start_date', trans('validation.attributes.start_date'), array('class' => 'control-label')) }}
-        {{ Form::text('start_date', $model->present()->dateOrNow('start_date'), array('class' => 'datepicker form-control', 'data-value' => $model->present()->dateOrNow('start_date'), 'placeholder' => trans('validation.attributes.DDMMYYYY'))) }}
-        {!! $errors->first('start_date', '<p class="help-block">:message</p>') !!}
+    <div class="col-sm-4">
+        {!! BootForm::date(trans('validation.attributes.start_date'), 'start_date')->value($model->present()->dateOrNow('start_date'))->placeholder(trans('validation.attributes.DDMMYYYY'))->addClass('datepicker') !!}
     </div>
-    <div class="col-sm-3 form-group @if($errors->has('start_time'))has-error @endif">
-        {{ Form::label('start_time', trans('validation.attributes.start_time'), array('class' => 'control-label')) }}
-        {{ Form::text('start_time', $model->present()->startTime, array('class' => 'form-control', 'placeholder' => trans('validation.attributes.HH:MM'))) }}
-        {!! $errors->first('start_time', '<p class="help-block">:message</p>') !!}
+    <div class="col-sm-3">
+        {!! BootForm::text(trans('validation.attributes.start_time'), 'start_time')->placeholder(trans('validation.attributes.HH:MM'))->addClass('timepicker') !!}
     </div>
 </div>
 
 <div class="row">
-    <div class="col-sm-4 form-group @if($errors->has('end_date'))has-error @endif">
-        {{ Form::label('end_date', trans('validation.attributes.end_date'), array('class' => 'control-label')) }}
-        {{ Form::text('end_date', $model->present()->dateOrNow('end_date'), array('class' => 'datepicker form-control', 'data-value' => $model->present()->dateOrNow('end_date'), 'placeholder' => trans('validation.attributes.DDMMYYYY'))) }}
-        {!! $errors->first('end_date', '<p class="help-block">:message</p>') !!}
+    <div class="col-sm-4">
+        {!! BootForm::date(trans('validation.attributes.end_date'), 'end_date')->value($model->present()->dateOrNow('end_date'))->placeholder(trans('validation.attributes.DDMMYYYY'))->addClass('datepicker') !!}
     </div>
-    <div class="col-sm-3 form-group @if($errors->has('end_time'))has-error @endif">
-        {{ Form::label('end_time', trans('validation.attributes.end_time'), array('class' => 'control-label')) }}
-        {{ Form::text('end_time', $model->present()->endTime, array('class' => 'form-control', 'placeholder' => trans('validation.attributes.HH:MM'))) }}
-        {!! $errors->first('end_time', '<p class="help-block">:message</p>') !!}
+    <div class="col-sm-3">
+        {!! BootForm::text(trans('validation.attributes.end_time'), 'end_time')->placeholder(trans('validation.attributes.HH:MM'))->addClass('timepicker') !!}
     </div>
 </div>
 
@@ -47,24 +39,10 @@
     @foreach ($locales as $lang)
 
     <div class="tab-pane fade @if ($locale == $lang)in active @endif" id="{{ $lang }}">
-        <div class="row">
-            <div class="col-md-6 form-group">
-                {!! BootForm::text(trans('labels.title'), $lang.'[title]') !!}
-            </div>
-            <div class="col-md-6 form-group @if($errors->has($lang.'.slug'))has-error @endif">
-                {{ Form::label($lang.'[slug]', trans('validation.attributes.slug'), array('class' => 'control-label')) }}
-                <div class="input-group">
-                    {{ Form::text($lang.'[slug]', $model->translate($lang)->slug, array('class' => 'form-control')) }}
-                    <span class="input-group-btn">
-                        <button class="btn btn-default btn-slug @if($errors->has($lang.'.slug'))btn-danger @endif" type="button">@lang('validation.attributes.generate')</button>
-                    </span>
-                </div>
-                {!! $errors->first($lang.'.slug', '<p class="help-block">:message</p>') !!}
-            </div>
-        </div>
-        {!! BootForm::checkbox(trans('labels.online'), $lang.'[status]') !!}
-        {!! BootForm::textarea(trans('labels.summary'), $lang.'[summary]')->addClass('editor')->rows(4) !!}
-        {!! BootForm::textarea(trans('labels.body'), $lang.'[body]')->addClass('editor') !!}
+        @include('core::admin._title-and-slug')
+        {!! BootForm::checkbox(trans('validation.attributes.online'), $lang.'[status]') !!}
+        {!! BootForm::textarea(trans('validation.attributes.summary'), $lang.'[summary]')->addClass('editor')->rows(4) !!}
+        {!! BootForm::textarea(trans('validation.attributes.body'), $lang.'[body]')->addClass('editor') !!}
     </div>
 
     @endforeach
