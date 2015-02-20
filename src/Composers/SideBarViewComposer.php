4280ef34d719f39fbdc5f5abeae255b91901fd22
Samuel De Backer <sdebacker@gmail.com>
2015-02-20 01:31:36 +0100

    licence + sidebar

--- src/Composers/SideBarViewComposer.php
+++ src/Composers/SideBarViewComposer.php
@@ -1,18 +1,25 @@
 <?php
 namespace TypiCMS\Modules\Events\Composers;
 
-use Illuminate\View\View;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Sidebar\SidebarGroup;
+use Maatwebsite\Sidebar\SidebarItem;
+use TypiCMS\Composers\BaseSidebarViewComposer;
 
-class SidebarViewComposer
+class SidebarViewComposer extends BaseSidebarViewComposer
 {
     public function compose(View $view)
     {
-        $view->menus['content']->put('events', [
-            'weight' => config('typicms.events.sidebar.weight'),
-            'request' => $view->prefix . '/events*',
-            'route' => 'admin.events.index',
-            'icon-class' => 'icon fa fa-fw fa-calendar',
-            'title' => 'Events',
-        ]);
+        $view->sidebar->group(trans('global.menus.content'), function (SidebarGroup $group) {
+            $group->addItem(trans('events::global.name'), function (SidebarItem $item) {
+                $item->icon = config('typicms.events.sidebar.icon', 'icon fa fa-fw fa-calendar');
+                $item->weight = config('typicms.events.sidebar.weight');
+                $item->route('admin.events.index');
+                $item->append('admin.events.create');
+                $item->authorize(
+                    $this->auth->hasAccess('events.index')
+                );
+            });
+        });
     }
 }
