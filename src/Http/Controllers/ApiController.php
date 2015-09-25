3e4e5f3c1714a42a6ba928ff5ccc8b9375a40181
Samuel De Backer <sdebacker@gmail.com>
2015-09-25 20:40:14 +0200

    …

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -1,8 +1,8 @@
 <?php
 namespace TypiCMS\Modules\Events\Http\Controllers;
 
+use Illuminate\Support\Facades\Input;
 use TypiCMS\Modules\Core\Http\Controllers\BaseApiController;
-use TypiCMS\Modules\Events\Http\Requests\FormRequest;
 use TypiCMS\Modules\Events\Repositories\EventInterface as Repository;
 
 class ApiController extends BaseApiController
@@ -15,10 +15,9 @@ class ApiController extends BaseApiController
     /**
      * Store a newly created resource in storage.
      *
-     * @param  FormRequest $request
      * @return Model|false
      */
-    public function store(FormRequest $request)
+    public function store()
     {
         $model = $this->repository->create(Input::all());
         $error = $model ? false : true ;
@@ -32,12 +31,11 @@ class ApiController extends BaseApiController
      * Update the specified resource in storage.
      *
      * @param  $model
-     * @param  FormRequest $request
      * @return boolean
      */
-    public function update($model, FormRequest $request)
+    public function update($model)
     {
-        $error = $this->repository->update($request->all()) ? false : true ;
+        $error = $this->repository->update(Input::all()) ? false : true ;
         return response()->json([
             'error' => $error,
         ], 200);
