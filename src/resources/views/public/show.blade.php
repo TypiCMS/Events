9ca49be1a0862762a3619b46dc082825cda0dfa1
Samuel De Backer <sdebacker@gmail.com>
2015-11-30 20:44:00 +0100

    event

--- src/resources/views/public/show.blade.php
+++ src/resources/views/public/show.blade.php
@@ -11,19 +11,19 @@
     @include('core::public._btn-prev-next', ['module' => 'Events', 'model' => $model])
 
     <article class="event" itemscope itemtype="http://schema.org/Event">
-        <h1 itemprop="name">{{ $model->title }}</h1>
         <link itemprop="url" href="{{ route($lang.'.events.slug', $model->slug) }}">
         <meta itemprop="startDate" content="{{ $model->start_date->toIso8601String() }}">
         <meta itemprop="endDate" content="{{ $model->end_date->toIso8601String() }}">
         <meta itemprop="image" content="{{ $model->present()->thumbAbsoluteSrc() }}">
-        {!! $model->present()->thumb(null, 200) !!}
-        <div itemprop="location">{{ $model->location }}</div>
-        <div class="date">{!! $model->present()->dateFromTo !!} <br>{!! $model->present()->timeFromTo !!}</div>
-        <p itemprop="description" class="summary">{{ nl2br($model->summary) }}</p>
+        <h1 class="event-title" itemprop="name">{{ $model->title }}</h1>
+        <div class="event-date">{!! $model->present()->dateFromTo !!} <br>{!! $model->present()->timeFromTo !!}</div>
+        <div class="event-location" itemprop="location">{{ $model->location }}</div>
+        <p class="event-summary" itemprop="description">{{ nl2br($model->summary) }}</p>
         <a class="btn btn-default btn-xs" href="{{ route($lang.'.events.slug.ics', $model->slug) }}">
             <span class="fa fa-calendar"></span> @lang('db.Add to calendar')
         </a>
-        <div class="body">{!! $model->present()->body !!}</div>
+        <div class="event-body">{!! $model->present()->body !!}</div>
+        {!! $model->present()->thumb() !!}
     </article>
 
 @stop
