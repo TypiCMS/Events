e5075e9b3bc12bf573eef1a9d47eb034e3aa8abe
Samuel De Backer <sdebacker@gmail.com>
2015-11-30 19:49:14 +0100

    microdata in list

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,14 +1,16 @@
-<li class="event">
-    <a href="{{ route($lang . '.events.slug', $event->slug) }}">
+<li class="event" itemscope itemtype="http://schema.org/Event">
+    <a href="{{ route($lang . '.events.slug', $event->slug) }}" itemprop="url">
+        <meta itemprop="startDate" content="{{ $event->start_date->toIso8601String() }}">
+        <meta itemprop="endDate" content="{{ $event->end_date->toIso8601String() }}">
+        <meta itemprop="image" content="{{ $event->present()->thumbAbsoluteSrc() }}">
         <div class="event-image">
             {!! $event->present()->thumb(540, 400) !!}
         </div>
         <div class="event-info">
-            <div class="event-title">{{ $event->title }}</div>
-            <div class="event-price">{{ $event->price }} {{ $event->currency }}</div>
-            <div class="event-location">{{ $event->location }}</div>
-            <div class="event-summary">{{ $event->summary }}</div>
+            <div class="event-title" itemprop="name">{{ $event->title }}</div>
             <div class="event-date">{!! $event->present()->dateFromTo !!}</div>
+            <div class="event-location" itemprop="location">{{ $event->location }}</div>
+            <div class="event-summary" itemprop="description">{{ $event->summary }}</div>
         </div>
     </a>
 </li>
