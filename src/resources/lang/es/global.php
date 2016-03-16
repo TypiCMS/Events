9f05868e8281e1afc1d5dd889863935052e3a0f2
Rafael Perelló Rowley <rperellorow@gmail.com>
2016-03-16 12:20:18 +0100

    Added translation to spanish

---
+++ src/resources/lang/es/global.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name'   => 'Eventos',
+    'events' => 'evento|eventos',
+    'New'    => 'Nuevo evento',
+    'Edit'   => 'Editar evento',
+    'Back'   => 'Volver a los eventos',
+    'from'   => 'desde ',
+    'to'     => ' a ',
+    'on'     => 'la ',
+];
