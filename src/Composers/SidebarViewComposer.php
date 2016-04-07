cc7beb64c07bf9a50c4eab9897aae4fa1abc78ec
Samuel De Backer <sdebacker@gmail.com>
2016-04-07 11:21:39 +0200

    Gate in place of auth

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -3,6 +3,7 @@
 namespace TypiCMS\Modules\Events\Composers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Gate;
 use Maatwebsite\Sidebar\SidebarGroup;
 use Maatwebsite\Sidebar\SidebarItem;
 
@@ -17,7 +18,7 @@ class SidebarViewComposer
                 $item->route('admin::index-events');
                 $item->append('admin::create-events');
                 $item->authorize(
-                    auth()->user()->can('index-events')
+                    Gate::allows('index-events')
                 );
             });
         });
