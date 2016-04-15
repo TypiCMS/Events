03ab6c4535debf60918191cd996b1ba55e7aebce
Samuel De Backer <sdebacker@gmail.com>
2016-04-15 16:41:11 +0200

    prepare for spatie translatable

--- src/Models/Event.php
+++ src/Models/Event.php
@@ -2,28 +2,24 @@
 
 namespace TypiCMS\Modules\Events\Models;
 
-use Dimsav\Translatable\Translatable;
 use Laracasts\Presenter\PresentableTrait;
+use Spatie\Translatable\HasTranslations;
 use TypiCMS\Modules\Core\Models\Base;
 use TypiCMS\Modules\History\Traits\Historable;
 
 class Event extends Base
 {
+    use HasTranslations;
     use Historable;
-    use Translatable;
     use PresentableTrait;
 
     protected $presenter = 'TypiCMS\Modules\Events\Presenters\ModulePresenter';
 
     protected $dates = ['start_date', 'end_date'];
 
-    protected $fillable = [
-        'start_date',
-        'end_date',
-        'start_time',
-        'end_time',
-        'image',
-        // Translatable columns
+    protected $guarded = ['id'];
+
+    public $translatable = [
         'title',
         'slug',
         'status',
@@ -33,51 +29,11 @@ class Event extends Base
         'body',
     ];
 
-    /**
-     * Translatable model configs.
-     *
-     * @var array
-     */
-    public $translatedAttributes = [
-        'title',
-        'slug',
-        'status',
-        'venue',
-        'address',
-        'summary',
-        'body',
-    ];
+    protected $appends = ['thumb'];
 
-    protected $appends = ['status', 'title', 'thumb'];
-
-    /**
-     * Columns that are file.
-     *
-     * @var array
-     */
     public $attachments = [
         'image',
     ];
-
-    /**
-     * Append status attribute from translation table.
-     *
-     * @return string
-     */
-    public function getStatusAttribute()
-    {
-        return $this->status;
-    }
-
-    /**
-     * Append title attribute from translation table.
-     *
-     * @return string title
-     */
-    public function getTitleAttribute()
-    {
-        return $this->title;
-    }
 
     /**
      * Append thumb attribute.
--- src/database/migrations/2013_08_31_000501_create_events_table.php
+++ src/database/migrations/2013_08_31_000501_create_events_table.php
@@ -18,25 +18,14 @@ class CreateEventsTable extends Migration
             $table->datetime('start_date');
             $table->datetime('end_date')->nullable();
             $table->string('image')->nullable();
+            $table->json('status');
+            $table->json('title');
+            $table->json('slug');
+            $table->json('venue');
+            $table->json('address');
+            $table->json('summary');
+            $table->json('body');
             $table->timestamps();
-        });
-
-        Schema::create('event_translations', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
-            $table->increments('id');
-            $table->integer('event_id')->unsigned();
-            $table->string('locale');
-            $table->boolean('status')->default(0);
-            $table->string('title');
-            $table->string('slug')->nullable();
-            $table->string('venue')->nullable();
-            $table->text('address');
-            $table->text('summary');
-            $table->text('body');
-            $table->timestamps();
-            $table->unique(['event_id', 'locale']);
-            $table->unique(['locale', 'slug']);
-            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
         });
     }
 
@@ -47,7 +36,6 @@ class CreateEventsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('event_translations');
         Schema::drop('events');
     }
 }
