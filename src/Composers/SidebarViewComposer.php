6a3ad06e5eff997e82250159abbb375d63fa5eab
Samuel De Backer <sdebacker@gmail.com>
2016-07-05 10:26:37 +0200

    sidebar item id

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -13,6 +13,7 @@ class SidebarViewComposer
     {
         $view->sidebar->group(trans('global.menus.content'), function (SidebarGroup $group) {
             $group->addItem(trans('events::global.name'), function (SidebarItem $item) {
+                $item->id = 'events';
                 $item->icon = config('typicms.events.sidebar.icon', 'icon fa fa-fw fa-calendar');
                 $item->weight = config('typicms.events.sidebar.weight');
                 $item->route('admin::index-events');
