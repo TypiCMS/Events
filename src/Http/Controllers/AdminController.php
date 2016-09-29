f2d178a0c1533220824c96f88f78efe43b3e38ef
Samuel De Backer <sdebacker@gmail.com>
2016-09-29 17:33:05 +0200

    createModel in place of getModel

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -34,7 +34,7 @@ class AdminController extends BaseAdminController
      */
     public function create()
     {
-        $model = $this->repository->getModel();
+        $model = $this->repository->createModel();
 
         return view('events::admin.create')
             ->with(compact('model'));
