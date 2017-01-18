ebe213555ba4143ffbf9e86e3d291b9fd8d8aa85
Samuel De Backer <sdebacker@gmail.com>
2017-01-18 22:50:33 +0100

    locales() helper method

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -32,7 +32,7 @@ class RouteServiceProvider extends ServiceProvider
              */
             if ($page = TypiCMS::getPageLinkedToModule('events')) {
                 $options = $page->private ? ['middleware' => 'auth'] : [];
-                foreach (config('translatable-bootforms.locales') as $lang) {
+                foreach (locales() as $lang) {
                     if ($page->translate('status', $lang) && $uri = $page->uri($lang)) {
                         $router->get($uri, $options + ['as' => $lang.'.events', 'uses' => 'PublicController@index']);
                         $router->get($uri.'/{slug}', $options + ['as' => $lang.'.events.slug', 'uses' => 'PublicController@show']);
