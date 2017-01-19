e5ae500525456ceba4af5f4be99e1d91e1ddd549
Samuel De Backer <sdebacker@gmail.com>
2017-01-19 10:21:16 +0100

    double arrow alignment removed

--- src/config/config.php
+++ src/config/config.php
@@ -2,8 +2,8 @@
 
 return [
     'per_page' => 50,
-    'select'   => ['id', 'title', 'image', 'start_date', 'end_date', 'status'],
-    'order'    => [
+    'select' => ['id', 'title', 'image', 'start_date', 'end_date', 'status'],
+    'order' => [
         'end_date' => 'desc',
     ],
     'sidebar' => [
