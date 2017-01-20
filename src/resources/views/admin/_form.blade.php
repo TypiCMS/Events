33563ad6d43e290b2c682a8161e5b30fa1716fc6
Samuel De Backer <sdebacker@gmail.com>
2017-01-20 18:22:17 +0100

    validations attributes in a json file

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -10,18 +10,18 @@
 
 @include('core::form._title-and-slug')
 {!! TranslatableBootForm::hidden('status')->value(0) !!}
-{!! TranslatableBootForm::checkbox(__('validation.attributes.online'), 'status') !!}
+{!! TranslatableBootForm::checkbox(__('Online'), 'status') !!}
 
 <div class="row">
     <div class="col-sm-6">
-        {!! BootForm::text(__('validation.attributes.start_date'), 'start_date')->value(old('start_date') ? : $model->present()->datetimeOrNow('start_date'))->addClass('datetimepicker') !!}
+        {!! BootForm::text(__('Start date'), 'start_date')->value(old('start_date') ? : $model->present()->datetimeOrNow('start_date'))->addClass('datetimepicker') !!}
     </div>
     <div class="col-sm-6">
-        {!! BootForm::text(__('validation.attributes.end_date'), 'end_date')->value(old('end_date') ? : $model->present()->datetimeOrNow('end_date'))->addClass('datetimepicker') !!}
+        {!! BootForm::text(__('End date'), 'end_date')->value(old('end_date') ? : $model->present()->datetimeOrNow('end_date'))->addClass('datetimepicker') !!}
     </div>
 </div>
 
-{!! TranslatableBootForm::text(__('validation.attributes.venue'), 'venue') !!}
-{!! TranslatableBootForm::textarea(__('validation.attributes.address'), 'address')->rows(4) !!}
-{!! TranslatableBootForm::textarea(__('validation.attributes.summary'), 'summary')->rows(4) !!}
-{!! TranslatableBootForm::textarea(__('validation.attributes.body'), 'body')->addClass('ckeditor') !!}
+{!! TranslatableBootForm::text(__('Venue'), 'venue') !!}
+{!! TranslatableBootForm::textarea(__('Address'), 'address')->rows(4) !!}
+{!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
+{!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor') !!}
