d8374471e17d2b115ec280dc77782372a264e497
Samuel De Backer <sdebacker@gmail.com>
2017-01-23 20:16:20 +0100

    default classes

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,19 +1,19 @@
-<li class="event" itemscope itemtype="http://schema.org/Event">
+<li class="events-item" itemscope itemtype="http://schema.org/Event">
     <a href="{{ route($lang.'::event', $event->slug) }}" itemprop="url">
         <meta itemprop="startDate" content="{{ $event->start_date->toIso8601String() }}">
         <meta itemprop="endDate" content="{{ $event->end_date->toIso8601String() }}">
         <meta itemprop="image" content="{{ $event->present()->thumbUrl() }}">
-        <div class="event-image">
+        <div class="events-item-image">
             {!! $event->present()->thumb(540, 400) !!}
         </div>
-        <div class="event-info">
-            <div class="event-date">{!! $event->present()->dateFromTo !!}</div>
-            <div class="event-title" itemprop="name">{{ $event->title }}</div>
-            <div class="event-location" itemprop="location">
+        <div class="events-item-info">
+            <div class="events-item-date">{!! $event->present()->dateFromTo !!}</div>
+            <div class="events-item-title" itemprop="name">{{ $event->title }}</div>
+            <div class="events-item-location" itemprop="location">
                 <span itemprop="name">{{ $event->venue }}</span>
                 <div class="address" itemprop="address">{{ nl2br($event->address) }}</div>
             </div>
-            <div class="event-summary" itemprop="description">{{ $event->summary }}</div>
+            <div class="events-item-summary" itemprop="description">{{ $event->summary }}</div>
         </div>
     </a>
 </li>
