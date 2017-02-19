d43b626fa5b5a92c2ccc57a9c112e2bedf420eff
Samuel De Backer <sdebacker@gmail.com>
2017-02-19 22:52:09 +0100

    request() method

--- src/Http/Controllers/PublicController.php
+++ src/Http/Controllers/PublicController.php
@@ -2,7 +2,6 @@
 
 namespace TypiCMS\Modules\Events\Http\Controllers;
 
-use Illuminate\Support\Facades\Request;
 use TypiCMS;
 use TypiCMS\Modules\Core\Http\Controllers\BasePublicController;
 use TypiCMS\Modules\Events\Repositories\EloquentEvent;
@@ -25,7 +24,7 @@ class PublicController extends BasePublicController
      */
     public function index()
     {
-        $page = Request::input('page');
+        $page = request('page');
         $perPage = config('typicms.events.per_page');
         $models = $this->repository->published()->paginate($perPage, ['*'], 'page', $page);
 
