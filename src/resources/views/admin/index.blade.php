1b1c6b9a5ff7535399da4f8246be8f3c4ce5c4af
Samuel De Backer <sdebacker@gmail.com>
2017-04-14 11:51:02 +0200

    Export button removed

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -15,7 +15,6 @@
     <div class="btn-toolbar">
         @include('core::admin._button-select')
         @include('core::admin._button-actions')
-        @include('core::admin._button-export')
         @include('core::admin._lang-switcher-for-list')
     </div>
 
