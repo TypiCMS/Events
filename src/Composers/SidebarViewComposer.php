b3ceb80cb2d34ac01e7111d36fc3bf354b298b4f
Samuel De Backer <sdebacker@gmail.com>
2017-04-18 16:34:34 +0200

    sidebar group translation

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -11,7 +11,7 @@ class SidebarViewComposer
 {
     public function compose(View $view)
     {
-        $view->sidebar->group(__('global.menus.content'), function (SidebarGroup $group) {
+        $view->sidebar->group(__('Content'), function (SidebarGroup $group) {
             $group->addItem(__('events::global.name'), function (SidebarItem $item) {
                 $item->id = 'events';
                 $item->icon = config('typicms.events.sidebar.icon', 'icon fa fa-fw fa-calendar');
