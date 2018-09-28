496a9cf717570d617bae4ac5fbbccc55ef2f70ff
Samuel De Backer <sdebacker@gmail.com>
2018-09-28 17:30:41 +0200

    header markup

--- src/resources/views/admin/create.blade.php
+++ src/resources/views/admin/create.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
 
-    @include('core::admin._button-back', ['module' => 'events'])
-    <h1>
-        @lang('New event')
-    </h1>
+    <div class="header">
+        @include('core::admin._button-back', ['module' => 'events'])
+        <h1 class="header-title">@lang('New event')</h1>
+    </div>
 
     {!! BootForm::open()->action(route('admin::index-events'))->multipart()->role('form') !!}
         @include('events::admin._form')
