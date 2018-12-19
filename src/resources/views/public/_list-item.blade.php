d62da242539f746e38fdec55cc82160b8bdea031
Samuel De Backer <sdebacker@gmail.com>
2018-12-19 12:24:10 +0100

    ->uri() call in place of route() helper

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,5 +1,5 @@
 <li class="events-item" itemscope itemtype="http://schema.org/Event">
-    <a class="events-item-link" href="{{ route($lang.'::event', $event->slug) }}" itemprop="url">
+    <a class="events-item-link" href="{{ $event->uri() }}" itemprop="url">
         <meta itemprop="startDate" content="{{ $event->start_date->toIso8601String() }}">
         <meta itemprop="endDate" content="{{ $event->end_date->toIso8601String() }}">
         <meta itemprop="image" content="{{ $event->present()->thumbUrl() }}">
