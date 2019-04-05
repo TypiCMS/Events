27da5344f93b8ba7a2dc89cb9f75710f03f9f5d3
Samuel De Backer <sdebacker@gmail.com>
2019-04-06 00:02:18 +0200

    Microdata removed

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,19 +1,16 @@
-<li class="event-list-item" itemscope itemtype="http://schema.org/Event">
-    <a class="event-list-item-link" href="{{ $event->uri() }}" itemprop="url">
-        <meta itemprop="startDate" content="{{ $event->start_date->toIso8601String() }}">
-        <meta itemprop="endDate" content="{{ $event->end_date->toIso8601String() }}">
-        <meta itemprop="image" content="{{ $event->present()->image() }}">
-        <div class="event-list-item-image">
-            <img src="{!! $event->present()->image(540, 400) !!}" alt="">
+<li class="event-list-item">
+    <a class="event-list-item-link" href="{{ $event->uri() }}">
+        <div class="event-list-item-image-wrapper">
+            <img class="event-list-item-image" src="{!! $event->present()->image(540, 400) !!}" alt="">
         </div>
         <div class="event-list-item-info">
             <div class="event-list-item-date">{!! $event->present()->dateFromTo !!}</div>
-            <div class="event-list-item-title" itemprop="name">{{ $event->title }}</div>
-            <div class="event-list-item-location" itemprop="location">
-                <span itemprop="name">{{ $event->venue }}</span>
-                <div class="address" itemprop="address">{{ nl2br($event->address) }}</div>
+            <div class="event-list-item-title">{{ $event->title }}</div>
+            <div class="event-list-item-location">
+                <span class="event-list-item-venue">{{ $event->venue }}</span>
+                <div class="event-list-item-address">{{ nl2br($event->address) }}</div>
             </div>
-            <div class="event-list-item-summary" itemprop="description">{{ $event->summary }}</div>
+            <div class="event-list-item-summary">{{ $event->summary }}</div>
         </div>
     </a>
 </li>
--- src/resources/views/public/show.blade.php
+++ src/resources/views/public/show.blade.php
@@ -10,23 +10,19 @@
 
     @include('core::public._btn-prev-next', ['module' => 'Events', 'model' => $model])
 
-    <article class="event" itemscope itemtype="http://schema.org/Event">
-        <link itemprop="url" href="{{ route($lang.'::event', $model->slug) }}">
-        <meta itemprop="startDate" content="{{ $model->start_date->toIso8601String() }}">
-        <meta itemprop="endDate" content="{{ $model->end_date->toIso8601String() }}">
-        <meta itemprop="image" content="{{ $model->present()->image() }}">
-        <h1 class="event-title" itemprop="name">{{ $model->title }}</h1>
+    <article class="event">
+        <h1 class="event-title">{{ $model->title }}</h1>
         <div class="event-date">{!! $model->present()->dateFromTo !!} <br>{!! $model->present()->timeFromTo !!}</div>
-        <div class="event-location" itemprop="location">
-            <span itemprop="name">{{ $model->venue }}</span>
-            <div class="address" itemprop="address">{!! nl2br($model->address) !!}</div>
+        <div class="event-location">
+            <span class="event-venue">{{ $model->venue }}</span>
+            <div class="event-address">{!! nl2br($model->address) !!}</div>
         </div>
-        <p class="event-summary" itemprop="description">{!! nl2br($model->summary) !!}</p>
+        <p class="event-summary">{!! nl2br($model->summary) !!}</p>
         <a class="btn btn-light btn-xs" href="{{ route($lang.'::event-ics', $model->slug) }}">
             <span class="fa fa-calendar"></span> @lang('db.Add to calendar')
         </a>
         <div class="event-body">{!! $model->present()->body !!}</div>
-        <img src="{!! $model->present()->image(2000) !!}" alt="">
+        <img class="event-image" src="{!! $model->present()->image(2000) !!}" alt="">
         @include('files::public._documents')
         @include('files::public._images')
     </article>
