248529148145d28c5d6acb122bbfd66ca6cc3224
Samuel De Backer <sdebacker@gmail.com>
2020-03-21 13:18:27 +0100

    slug validation rule

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -13,7 +13,7 @@ class FormRequest extends AbstractFormRequest
             'end_date' => 'required|date_format:"Y-m-d\TH:i"|after_or_equal:start_date',
             'image_id' => 'nullable|integer',
             'title.*' => 'nullable|max:255',
-            'slug.*' => 'nullable|alpha_dash|max:255|required_with:title.*',
+            'slug.*' => 'nullable|alpha_dash|max:255|required_if:status.*,1|required_with:title.*',
             'venue.*' => 'nullable|max:255',
         ];
     }
