70f1bfbcf1eb01376073be9d8598749ae94d3f74
Samuel De Backer <sdebacker@gmail.com>
2020-07-17 23:44:02 +0200

    <file-manager> in place of <filepicker>

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -8,7 +8,7 @@
 
 {!! BootForm::hidden('id') !!}
 
-<filepicker related-table="{{ $model->getTable() }}" :related-id="{{ $model->id ?? 0 }}"></filepicker>
+<file-manager related-table="{{ $model->getTable() }}" :related-id="{{ $model->id ?? 0 }}"></file-manager>
 <file-field type="image" field="image_id" data="{{ $model->image }}"></file-field>
 <files related-table="{{ $model->getTable() }}" :related-id="{{ $model->id ?? 0 }}"></files>
 
