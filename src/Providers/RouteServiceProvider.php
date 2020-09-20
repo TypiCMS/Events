4f9949a87eba3fb3eb81190ebf08ff3a12eaeaee
Samuel De Backer <sdebacker@gmail.com>
2020-09-20 22:38:44 +0200

    Routes definition cleaned

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -17,46 +17,42 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
-        Route::namespace($this->namespace)->group(function (Router $router) {
-            /*
-             * Front office routes
-             */
-            if ($page = TypiCMS::getPageLinkedToModule('events')) {
-                $middleware = $page->private ? ['public', 'auth'] : ['public'];
-                $router->middleware($middleware)->group(function (Router $router) use ($page) {
-                    foreach (locales() as $lang) {
-                        if ($page->translate('status', $lang) && $uri = $page->uri($lang)) {
-                            $router->get($uri, [PublicController::class, 'index'])->name($lang.'::index-events');
-                            $router->get($uri.'/past', [PublicController::class, 'past'])->name($lang.'::past-events');
-                            $router->get($uri.'/{slug}', [PublicController::class, 'show'])->name($lang.'::event');
-                            $router->get($uri.'/{slug}/ics', [PublicController::class, 'ics'])->name($lang.'::event-ics');
-                        }
-                    }
-                });
+        /*
+         * Front office routes
+         */
+        if ($page = TypiCMS::getPageLinkedToModule('events')) {
+            $middleware = $page->private ? ['public', 'auth'] : ['public'];
+            foreach (locales() as $lang) {
+                if ($page->isPublished($lang) && $uri = $page->uri($lang)) {
+                    Route::middleware($middleware)->prefix($uri)->name($lang.'::')->group(function (Router $router) {
+                        $router->get('/', [PublicController::class, 'index'])->name('index-events');
+                        $router->get('past', [PublicController::class, 'past'])->name('past-events');
+                        $router->get('{slug}', [PublicController::class, 'show'])->name('event');
+                        $router->get('{slug}/ics', [PublicController::class, 'ics'])->name('event-ics');
+                    });
+                }
             }
+        }
 
-            /*
-             * Admin routes
-             */
-            $router->middleware('admin')->prefix('admin')->group(function (Router $router) {
-                $router->get('events', [AdminController::class, 'index'])->name('admin::index-events')->middleware('can:read events');
-                $router->get('events/create', [AdminController::class, 'create'])->name('admin::create-event')->middleware('can:create events');
-                $router->get('events/{event}/edit', [AdminController::class, 'edit'])->name('admin::edit-event')->middleware('can:read events');
-                $router->get('events/{event}/files', [AdminController::class, 'files'])->name('admin::edit-event-files')->middleware('can:update events');
-                $router->post('events', [AdminController::class, 'store'])->name('admin::store-event')->middleware('can:create events');
-                $router->put('events/{event}', [AdminController::class, 'update'])->name('admin::update-event')->middleware('can:update events');
-            });
+        /*
+         * Admin routes
+         */
+        Route::middleware('admin')->prefix('admin')->name('admin::')->group(function (Router $router) {
+            $router->get('events', [AdminController::class, 'index'])->name('index-events')->middleware('can:read events');
+            $router->get('events/create', [AdminController::class, 'create'])->name('create-event')->middleware('can:create events');
+            $router->get('events/{event}/edit', [AdminController::class, 'edit'])->name('edit-event')->middleware('can:read events');
+            $router->get('events/{event}/files', [AdminController::class, 'files'])->name('edit-event-files')->middleware('can:update events');
+            $router->post('events', [AdminController::class, 'store'])->name('store-event')->middleware('can:create events');
+            $router->put('events/{event}', [AdminController::class, 'update'])->name('update-event')->middleware('can:update events');
+        });
 
-            /*
-             * API routes
-             */
-            $router->middleware('api')->prefix('api')->group(function (Router $router) {
-                $router->middleware('auth:api')->group(function (Router $router) {
-                    $router->get('events', [ApiController::class, 'index'])->middleware('can:read events');
-                    $router->patch('events/{event}', [ApiController::class, 'updatePartial'])->middleware('can:update events');
-                    $router->delete('events/{event}', [ApiController::class, 'destroy'])->middleware('can:delete events');
-                });
-            });
+        /*
+         * API routes
+         */
+        Route::middleware(['api', 'auth:api'])->prefix('api')->group(function (Router $router) {
+            $router->get('events', [ApiController::class, 'index'])->middleware('can:read events');
+            $router->patch('events/{event}', [ApiController::class, 'updatePartial'])->middleware('can:update events');
+            $router->delete('events/{event}', [ApiController::class, 'destroy'])->middleware('can:delete events');
         });
     }
 }
