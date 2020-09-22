9642f5fefcee41252283adb7529b777938b183f8
Samuel De Backer <sdebacker@gmail.com>
2020-09-22 09:03:31 +0200

    dateFromTo() refactored

--- src/Presenters/ModulePresenter.php
+++ src/Presenters/ModulePresenter.php
@@ -6,107 +6,49 @@ use TypiCMS\Modules\Core\Presenters\Presenter;
 
 class ModulePresenter extends Presenter
 {
-    private $dateFormat = 'd.m.Y';
-
-    private $timeFormat = 'H:i';
+    private $dateFormat = '%e %B %Y';
 
     /**
      * Return formatted start_date.
-     *
-     * @return string
      */
-    public function startDate()
+    public function startDate(): string
     {
-        return $this->entity->start_date->format($this->dateFormat);
+        return $this->entity->start_date->formatLocalized($this->dateFormat);
     }
 
     /**
      * Return formatted end_date.
-     *
-     * @return string
      */
-    public function endDate()
+    public function endDate(): string
     {
-        return $this->entity->end_date->format($this->dateFormat);
+        return $this->entity->end_date->formatLocalized($this->dateFormat);
     }
 
     /**
-     * Return time formatted start_date.
-     *
-     * @return string
-     */
-    public function startTime()
-    {
-        if (!$this->entity->start_date) {
-            return '';
-        }
-
-        return $this->entity->start_date->format($this->timeFormat);
-    }
-
-    /**
-     * Return time formatted end_date.
-     *
-     * @return string
-     */
-    public function endTime()
-    {
-        if (!$this->entity->end_date) {
-            return '';
-        }
-
-        return $this->entity->end_date->format($this->timeFormat);
-    }
-
-    /**
-     * Format start + end date without repeating
+     * Format start and end date without repeating
      * month and year if they are the same.
-     *
-     * @return string html data
      */
-    public function dateFromTo()
+    public function dateFromTo(): string
     {
-        $sDate = $this->entity->start_date;
-        $eDate = $this->entity->end_date;
-        $dateFormat = '%e %B %Y';
-        $sDateFormat = $dateFormat;
-        if ($sDate->format('Ymd') == $eDate->format('Ymd')) {
+        $startDate = $this->entity->start_date;
+        $endDate = $this->entity->end_date;
+        $startDateFormat = $this->dateFormat;
+        if ($startDate->eq($endDate)) {
             return ucfirst(__('on')).' '.
-                '<time datetime="'.$sDate->toIso8601String().'">'.
-                $sDate->formatLocalized($dateFormat).
-                '</time>';
+                $startDate->formatLocalized($startDateFormat);
         }
-        if ($sDate->format('Y') == $eDate->format('Y')) {
-            $sDateFormat = '%e %B';
-            if ($sDate->format('m') == $eDate->format('m')) {
-                $sDateFormat = '%e';
+        if ($startDate->format('Y') === $endDate->format('Y')) {
+            $startDateFormat = '%e %B';
+            if ($startDate->format('m') === $endDate->format('m')) {
+                $startDateFormat = '%e';
             }
         }
 
         $dateFromTo = ucfirst(__('from')).' ';
-        $dateFromTo .= '<time datetime="'.$sDate->toIso8601String().'">';
-        $dateFromTo .= $sDate->formatLocalized($sDateFormat);
-        $dateFromTo .= '</time>';
+        $dateFromTo .= $startDate->formatLocalized($startDateFormat);
         $dateFromTo .= ' '.__('to').' ';
-        $dateFromTo .= '<time datetime="'.$eDate->toIso8601String().'">';
-        $dateFromTo .= $eDate->formatLocalized($dateFormat);
-        $dateFromTo .= '</time>';
+        $dateFromTo .= $endDate->formatLocalized($this->dateFormat);
 
         return $dateFromTo;
-    }
-
-    /**
-     * Format start and end time.
-     *
-     * @param $separator string
-     *
-     * @return string
-     */
-    public function timeFromTo($separator = ' - ')
-    {
-        $startTime = $this->entity->start_date->format($this->timeFormat);
-        $endTime = $this->entity->end_date->format($this->timeFormat);
-
-        return $startTime.$separator.$endTime;
     }
 }
