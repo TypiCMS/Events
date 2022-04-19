c22e5a47aea1cb4cf2d0e24c88aae6b80ac6af88
Samuel De Backer <sdebacker@gmail.com>
2022-04-19 21:34:26 +0200

    forms

--- resources/views/admin/_form.blade.php
+++ resources/views/admin/_form.blade.php
@@ -3,32 +3,40 @@
     <script src="{{ asset('components/ckeditor4/config-full.js') }}"></script>
 @endpush
 
-@component('core::admin._buttons-form', ['model' => $model])
-@endcomponent
-
-{!! BootForm::hidden('id') !!}
-
-<file-manager related-table="{{ $model->getTable() }}" :related-id="{{ $model->id ?? 0 }}"></file-manager>
-<file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
-<files-field :init-files="{{ $model->files }}"></files-field>
-
-@include('core::form._title-and-slug')
-<div class="mb-3">
-    {!! TranslatableBootForm::hidden('status')->value(0) !!}
-    {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+<div class="header">
+    @include('core::admin._button-back', ['url' => $model->indexUrl(), 'title' => __('Events')])
+    @include('core::admin._title', ['default' => __('New event')])
+    @component('core::admin._buttons-form', ['model' => $model])
+    @endcomponent
 </div>
 
-<div class="row gx-3">
-    <div class="col-sm-6">
-        {!! BootForm::date(__('Start date'), 'start_date')->value(old('start_date') ? : $model->present()->dateOrNow('start_date'))->required() !!}
-    </div>
-    <div class="col-sm-6">
-        {!! BootForm::date(__('End date'), 'end_date')->value(old('end_date') ? : $model->present()->dateOrNow('end_date'))->required() !!}
-    </div>
-</div>
+<div class="content">
 
-{!! TranslatableBootForm::text(__('Venue'), 'venue') !!}
-{!! TranslatableBootForm::textarea(__('Address'), 'address')->rows(3) !!}
-{!! TranslatableBootForm::text(__('Website'), 'website')->type('url')->placeholder('https://') !!}
-{!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
-{!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor-full') !!}
+    {!! BootForm::hidden('id') !!}
+
+    <file-manager related-table="{{ $model->getTable() }}" :related-id="{{ $model->id ?? 0 }}"></file-manager>
+    <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+    <files-field :init-files="{{ $model->files }}"></files-field>
+
+    @include('core::form._title-and-slug')
+    <div class="mb-3">
+        {!! TranslatableBootForm::hidden('status')->value(0) !!}
+        {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+    </div>
+
+    <div class="row gx-3">
+        <div class="col-sm-6">
+            {!! BootForm::date(__('Start date'), 'start_date')->value(old('start_date') ? : $model->present()->dateOrNow('start_date'))->required() !!}
+        </div>
+        <div class="col-sm-6">
+            {!! BootForm::date(__('End date'), 'end_date')->value(old('end_date') ? : $model->present()->dateOrNow('end_date'))->required() !!}
+        </div>
+    </div>
+
+    {!! TranslatableBootForm::text(__('Venue'), 'venue') !!}
+    {!! TranslatableBootForm::textarea(__('Address'), 'address')->rows(3) !!}
+    {!! TranslatableBootForm::text(__('Website'), 'website')->type('url')->placeholder('https://') !!}
+    {!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
+    {!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor-full') !!}
+
+</div>
--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -4,11 +4,6 @@
 
 @section('content')
 
-    <div class="header">
-        @include('core::admin._button-back', ['module' => 'events'])
-        <h1 class="header-title">@lang('New event')</h1>
-    </div>
-
     {!! BootForm::open()->action(route('admin::index-events'))->multipart()->role('form') !!}
         @include('events::admin._form')
     {!! BootForm::close() !!}
