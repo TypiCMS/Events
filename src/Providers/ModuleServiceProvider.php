d0c9f6999b3513f8db00e823e3a7ea9d3ce2ae5d
Samuel De Backer <sdebacker@gmail.com>
2022-04-22 09:57:15 +0200

    TypiCMS config

--- src/Providers/ModuleServiceProvider.php
+++ src/Providers/ModuleServiceProvider.php
@@ -15,7 +15,7 @@ class ModuleServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'typicms.modules.events');
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'typicms.modules');
 
         $this->loadViewsFrom(__DIR__.'/../../resources/views/', 'events');
 
