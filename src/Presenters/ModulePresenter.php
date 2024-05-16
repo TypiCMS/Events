a38a74a3b5b652de561e7536749e1f777cf75432
Samuel De Backer <sdebacker@gmail.com>
2024-05-16 17:51:13 +0200

    Carbon isoFormat method

--- src/Presenters/ModulePresenter.php
+++ src/Presenters/ModulePresenter.php
@@ -6,48 +6,33 @@ use TypiCMS\Modules\Core\Presenters\Presenter;
 
 class ModulePresenter extends Presenter
 {
-    private $dateFormat = '%e %B %Y';
-
-    /**
-     * Return formatted start_date.
-     */
-    public function startDate(): string
-    {
-        return $this->entity->start_date->formatLocalized($this->dateFormat);
-    }
-
-    /**
-     * Return formatted end_date.
-     */
-    public function endDate(): string
-    {
-        return $this->entity->end_date->formatLocalized($this->dateFormat);
-    }
-
     /**
      * Format start and end date without repeating
      * month and year if they are the same.
      */
-    public function dateFromTo(): string
+    public function dateFromTo(string $format = 'D MMMM'): string
     {
         $startDate = $this->entity->start_date;
         $endDate = $this->entity->end_date;
-        $startDateFormat = $this->dateFormat;
+        $showYear = false;
+        if ($startDate->format('Y') !== date('Y') || $endDate->format('Y') !== date('Y')) {
+            $showYear = true;
+        }
+        $startDateFormat = $format . ($showYear ? ' YYYY' : '');
+        $endDateFormat = $format . ($showYear ? ' YYYY' : '');
         if ($startDate->eq($endDate)) {
-            return ucfirst(__('on')) . ' ' .
-                $startDate->formatLocalized($startDateFormat);
+            return $startDate->isoFormat($startDateFormat);
         }
         if ($startDate->format('Y') === $endDate->format('Y')) {
-            $startDateFormat = '%e %B';
+            $startDateFormat = $format;
             if ($startDate->format('m') === $endDate->format('m')) {
-                $startDateFormat = '%e';
+                $startDateFormat = 'D';
             }
         }
 
-        $dateFromTo = ucfirst(__('from')) . ' ';
-        $dateFromTo .= $startDate->formatLocalized($startDateFormat);
-        $dateFromTo .= ' ' . __('to') . ' ';
-        $dateFromTo .= $endDate->formatLocalized($this->dateFormat);
+        $dateFromTo = $startDate->isoFormat($startDateFormat);
+        $dateFromTo .= ' → ';
+        $dateFromTo .= $endDate->isoFormat($endDateFormat);
 
         return $dateFromTo;
     }
