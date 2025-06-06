d4279b0ff0fda8b3e052f5c15c3fc29d65133705
Samuel De Backer <sdebacker@gmail.com>
2025-06-06 13:04:17 +0200

    PHPDoc

--- src/Models/Event.php
+++ src/Models/Event.php
@@ -3,18 +3,50 @@
 namespace TypiCMS\Modules\Events\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Route;
 use Laracasts\Presenter\PresentableTrait;
 use Spatie\Translatable\HasTranslations;
 use TypiCMS\Modules\Core\Models\Base;
 use TypiCMS\Modules\Core\Models\File;
+use TypiCMS\Modules\Core\Models\History;
 use TypiCMS\Modules\Core\Traits\HasFiles;
 use TypiCMS\Modules\Core\Traits\Historable;
 use TypiCMS\Modules\Events\Presenters\ModulePresenter;
 
+/**
+ * @property int $id
+ * @property Carbon $start_date
+ * @property Carbon $end_date
+ * @property int $registration_form
+ * @property int|null $og_image_id
+ * @property int|null $image_id
+ * @property array<array-key, mixed> $status
+ * @property array<array-key, mixed> $title
+ * @property array<array-key, mixed> $slug
+ * @property array<array-key, mixed> $venue
+ * @property array<array-key, mixed> $address
+ * @property array<array-key, mixed> $summary
+ * @property array<array-key, mixed> $body
+ * @property array<array-key, mixed> $website
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection<int, File> $audios
+ * @property-read Collection<int, File> $documents
+ * @property-read Collection<int, File> $files
+ * @property-read Collection<int, History> $history
+ * @property-read File|null $image
+ * @property-read Collection<int, File> $images
+ * @property-read File|null $ogImage
+ * @property-read Collection<int, Registration> $registrations
+ * @property-read mixed $thumb
+ * @property-read mixed $translations
+ * @property-read Collection<int, File> $videos
+ */
 class Event extends Base
 {
     use HasFiles;
@@ -109,16 +141,19 @@ class Event extends Base
         );
     }
 
-    public function image(): BelongsTo
-    {
-        return $this->belongsTo(File::class, 'image_id');
-    }
-
+    /** @return HasMany<Registration, $this> */
     public function registrations(): HasMany
     {
         return $this->hasMany(Registration::class);
     }
 
+    /** @return BelongsTo<File, $this> */
+    public function image(): BelongsTo
+    {
+        return $this->belongsTo(File::class, 'image_id');
+    }
+
+    /** @return BelongsTo<File, $this> */
     public function ogImage(): BelongsTo
     {
         return $this->belongsTo(File::class, 'og_image_id');
--- src/Models/Registration.php
+++ src/Models/Registration.php
@@ -2,14 +2,34 @@
 
 namespace TypiCMS\Modules\Events\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 use Laracasts\Presenter\PresentableTrait;
 use TypiCMS\Modules\Core\Models\Base;
+use TypiCMS\Modules\Core\Models\History;
 use TypiCMS\Modules\Core\Traits\Historable;
 use TypiCMS\Modules\Events\Presenters\RegistrationPresenter;
 
+/**
+ * @property int $id
+ * @property int $event_id
+ * @property int|null $user_id
+ * @property int $number_of_people
+ * @property string|null $email
+ * @property string|null $first_name
+ * @property string|null $last_name
+ * @property string|null $locale
+ * @property string|null $message
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Event $event
+ * @property-read Collection<int, History> $history
+ * @property-read int|null $history_count
+ * @property-write mixed $status
+ */
 class Registration extends Base
 {
     use Historable;
@@ -39,6 +59,7 @@ class Registration extends Base
         return route('dashboard');
     }
 
+    /** @return BelongsTo<Event, $this> */
     public function event(): BelongsTo
     {
         return $this->belongsTo(Event::class);
