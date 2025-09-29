9493782e1af6ce8ccf89825a5090e88421849d7e
Samuel De Backer <sdebacker@gmail.com>
2025-09-29 15:02:20 +0200

    pint

--- src/routes/events.php
+++ src/routes/events.php
@@ -1,8 +1,8 @@
 <?php
 
-use TypiCMS\Modules\Core\Models\Page;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
+use TypiCMS\Modules\Core\Models\Page;
 use TypiCMS\Modules\Events\Http\Controllers\AdminController;
 use TypiCMS\Modules\Events\Http\Controllers\ApiController;
 use TypiCMS\Modules\Events\Http\Controllers\PublicController;
