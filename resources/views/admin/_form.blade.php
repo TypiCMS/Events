2dcdc5e75d33c38fa2fa0c686618e2e20d136928
Samuel De Backer <sdebacker@gmail.com>
2025-10-16 13:05:43 +0200

    Update _form.blade.php

--- resources/views/admin/_form.blade.php
+++ resources/views/admin/_form.blade.php
@@ -7,31 +7,38 @@
 <div class="content">
     <x-core::form-errors />
 
-    <file-manager></file-manager>
-    <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
-    <file-field type="image" field="og_image_id" :init-file="{{ $model->ogImage ?? 'null' }}" label="Open Graph image"></file-field>
-    <files-field :init-files="{{ $model->files }}"></files-field>
+    <div class="row">
+        <div class="col-lg-8">
+            <x-core::title-and-slug-fields :locales="locales()" />
+            <div class="mb-3">
+                {!! TranslatableBootForm::hidden('status')->value(0) !!}
+                {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+                {!! BootForm::hidden('registration_form')->value(0) !!}
+                {!! BootForm::checkbox(__('Registration form'), 'registration_form') !!}
+            </div>
 
-    <x-core::title-and-slug-fields :locales="locales()" />
-    <div class="mb-3">
-        {!! TranslatableBootForm::hidden('status')->value(0) !!}
-        {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
-        {!! BootForm::hidden('registration_form')->value(0) !!}
-        {!! BootForm::checkbox(__('Registration form'), 'registration_form') !!}
-    </div>
+            <div class="row gx-3">
+                <div class="col-sm-6">
+                    {!! BootForm::date(__('Start date'), 'start_date')->value(old('start_date') ?: $model->present()->dateOrNow('start_date'))->required() !!}
+                </div>
+                <div class="col-sm-6">
+                    {!! BootForm::date(__('End date'), 'end_date')->value(old('end_date') ?: $model->present()->dateOrNow('end_date'))->required() !!}
+                </div>
+            </div>
 
-    <div class="row gx-3">
-        <div class="col-sm-6">
-            {!! BootForm::date(__('Start date'), 'start_date')->value(old('start_date') ?: $model->present()->dateOrNow('start_date'))->required() !!}
+            {!! TranslatableBootForm::text(__('Venue'), 'venue') !!}
+            {!! TranslatableBootForm::textarea(__('Address'), 'address')->rows(3) !!}
+            {!! TranslatableBootForm::text(__('Website'), 'website')->type('url')->placeholder('https://') !!}
+            {!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
+            <x-core::tiptap-editors :model="$model" name="body" :label="__('Body')" />
         </div>
-        <div class="col-sm-6">
-            {!! BootForm::date(__('End date'), 'end_date')->value(old('end_date') ?: $model->present()->dateOrNow('end_date'))->required() !!}
+        <div class="col-lg-4">
+            <div class="right-column">
+                <file-manager></file-manager>
+                <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+                <file-field type="image" field="og_image_id" :init-file="{{ $model->ogImage ?? 'null' }}" label="Open Graph image"></file-field>
+                <files-field :init-files="{{ $model->files }}"></files-field>
+            </div>
         </div>
     </div>
-
-    {!! TranslatableBootForm::text(__('Venue'), 'venue') !!}
-    {!! TranslatableBootForm::textarea(__('Address'), 'address')->rows(3) !!}
-    {!! TranslatableBootForm::text(__('Website'), 'website')->type('url')->placeholder('https://') !!}
-    {!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
-    <x-core::tiptap-editors :model="$model" name="body" :label="__('Body')" />
 </div>
