c201bec98885becea2293afed114e11009010129
Samuel De Backer <sdebacker@gmail.com>
2025-11-02 20:24:53 +0100

    Validation rules specified as arrays.

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -6,23 +6,23 @@ use TypiCMS\Modules\Core\Http\Requests\AbstractFormRequest;
 
 class FormRequest extends AbstractFormRequest
 {
-    /** @return array<string, string> */
+    /** @return array<string, list<string>> */
     public function rules(): array
     {
         return [
-            'start_date' => 'required|date_format:Y-m-d',
-            'end_date' => 'required|date_format:Y-m-d|after_or_equal:start_date',
-            'registration_form' => 'boolean',
-            'image_id' => 'nullable|integer',
-            'og_image_id' => 'nullable|integer',
-            'title.*' => 'nullable|max:255',
-            'slug.*' => 'nullable|alpha_dash|max:255|required_if:status.*,1|required_with:title.*',
-            'venue.*' => 'nullable|max:255',
-            'address.*' => 'nullable|max:1000',
-            'website.*' => 'nullable|max:1000|url',
-            'status.*' => 'boolean',
-            'summary.*' => 'nullable|max:1000',
-            'body.*' => 'nullable|max:20000',
+            'start_date' => ['required', 'date_format:Y-m-d'],
+            'end_date' => ['required', 'date_format:Y-m-d', 'after_or_equal:start_date'],
+            'registration_form' => ['boolean'],
+            'image_id' => ['nullable', 'integer'],
+            'og_image_id' => ['nullable', 'integer'],
+            'title.*' => ['nullable', 'max:255'],
+            'slug.*' => ['nullable', 'alpha_dash', 'max:255', 'required_if:status.*,1', 'required_with:title.*'],
+            'venue.*' => ['nullable', 'max:255'],
+            'address.*' => ['nullable', 'max:1000'],
+            'website.*' => ['nullable', 'max:1000', 'url'],
+            'status.*' => ['boolean'],
+            'summary.*' => ['nullable', 'max:1000'],
+            'body.*' => ['nullable', 'max:20000'],
         ];
     }
 }
--- src/Http/Requests/RegistrationFormRequest.php
+++ src/Http/Requests/RegistrationFormRequest.php
@@ -6,14 +6,14 @@ use TypiCMS\Modules\Core\Http\Requests\AbstractFormRequest;
 
 class RegistrationFormRequest extends AbstractFormRequest
 {
-    /** @return array<string, string> */
+    /** @return array<string, list<string>> */
     public function rules(): array
     {
         return [
-            'number_of_people' => 'required|integer|min:1',
-            'message' => 'nullable|max:5000',
-            'my_name' => 'honeypot',
-            'my_time' => 'required|honeytime:5',
+            'number_of_people' => ['required', 'integer', 'min:1'],
+            'message' => ['nullable', 'max:5000'],
+            'my_name' => ['honeypot'],
+            'my_time' => ['required', 'honeytime:5'],
         ];
     }
 }
